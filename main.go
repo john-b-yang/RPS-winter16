97bf92f84d39abcbe27ad833b69c7a342178d9dc
John Yang <byjohnyang@gmail.com>
2016-12-21 10:45:55 -0800

    Dockerfile sample and beginning project

---
+++ main.go
@@ -0,0 +1,27 @@
+/*
+ * Designated import statements and necessary packages
+ */
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+	"flag"
+)
+
+func main() {
+    temp := flag.String("Parameter Name", "Filler", "Prompt")
+
+    flag.Parse()
+}
+
+func client() {
+
+}
+
+func server() {
+
+}
